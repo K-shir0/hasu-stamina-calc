227ea33a7f586538699982b9519cb44fd228204c
k-shir0 <kakishiro2190394@gmail.com>
2024-12-05 21:13:25 +0900

    fix

--- src/hasu_stamina_calc/app.py
+++ src/hasu_stamina_calc/app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 # タイトル
 st.title('スクステスタミナ計算機')
 
